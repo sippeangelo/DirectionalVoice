28417f03c475f0c62a58372bec783a05f89e83e6
Simon Holmberg <holmberg.simon@gmail.com>
2020-11-10 18:43:07 +0100

    Initial testing version

---
+++ DirectionalVoiceMod.cs
@@ -0,0 +1,87 @@
+﻿using MelonLoader;
+using UnityEngine;
+using VRC;
+
+namespace DirectionalVoice
+{
+  public static class BuildInfo
+  {
+    public const string Name = "DirectionalVoice";
+    public const string Author = "Jace";
+    public const string Version = "0.1.0";
+    public const string DownloadLink = "https://gitlab.com/sippeangelo/DirectionalVoice";
+  }
+
+  public class DirectionalVoiceMod : MelonMod
+  {
+    private bool mEnabled;
+    private float mMinVolume;
+#if DEBUG
+    private string mDebugTarget;
+#endif
+
+    public override void OnApplicationStart()
+    {
+      MelonPrefs.RegisterCategory("DirectionalVoice", "Directional Voice");
+
+      MelonPrefs.RegisterBool("DirectionalVoice", "Enabled", true, "Directional Voice");
+      MelonPrefs.RegisterFloat("DirectionalVoice", "MinVolume", 25f, "Minimum volume when behind speaker (percent)");
+#if DEBUG
+      MelonPrefs.RegisterString("DirectionalVoice", "DebugTarget", "", "Debug Target");
+#endif
+
+      OnModSettingsApplied();
+    }
+
+    public override void OnUpdate()
+    {
+      if (!mEnabled) {
+        return;
+      }
+
+      foreach (Player player in Util.GetPlayers()) {
+        if (player == null || player.IsLocal()) {
+          continue;
+        }
+
+        Transform head = player.prop_VRCPlayer_0?.field_Internal_Animator_0?.GetBoneTransform(HumanBodyBones.Head);
+        if (!head) {
+          continue;
+        }
+
+        // Calculate the volume as a function of the angle between our postition and the other player's head rotation. Our own rotation shouldn't have an effect on the volume.
+        Vector3 directionToAvatar = Vector3.Normalize(Util.GetLocalPlayer().transform.position - head.position);
+        Vector3 avatarForward = head.forward;
+        float factor = (Vector3.Dot(directionToAvatar, avatarForward) + 1) / 2f;
+        float volume = 1 - (1 - factor) * (1 - mMinVolume / 100f);
+
+        player.field_Private_USpeaker_0.SpeakerVolume = volume;
+
+#if DEBUG
+        if (mDebugTarget.Length > 0 && player.prop_APIUser_0.displayName.ToLower().Contains(mDebugTarget.ToLower())) {
+          MelonLogger.Log($"directionToAvatar: {directionToAvatar.ToString()}");
+          MelonLogger.Log($"avatarForward: {avatarForward.ToString()}");
+          MelonLogger.Log($"Dot: {Vector3.Dot(directionToAvatar, avatarForward)} ({factor}), vol: {volume}");
+        }
+#endif
+      }
+    }
+
+    public override void OnModSettingsApplied()
+    {
+      bool enabled = MelonPrefs.GetBool("DirectionalVoice", "Enabled");
+      if (!enabled && mEnabled) {
+        foreach (Player player in Util.GetPlayers()) {
+          player.field_Private_USpeaker_0.SpeakerVolume = 1f;
+        }
+      }
+      mEnabled = enabled;
+
+      mMinVolume = MelonPrefs.GetFloat("DirectionalVoice", "MinVolume");
+
+#if DEBUG
+      mDebugTarget = MelonPrefs.GetString("DirectionalVoice", "DebugTarget");
+#endif
+    }
+  }
+}
---
+++ Util.cs
@@ -0,0 +1,30 @@
+﻿using Il2CppSystem.Collections.Generic;
+using UnityEngine;
+using VRC;
+
+namespace DirectionalVoice
+{
+  static class Util
+  {
+    public static PlayerManager GetPlayerManager() =>PlayerManager.prop_PlayerManager_0;
+
+    public static VRCPlayer GetLocalPlayer() => VRCPlayer.field_Internal_Static_VRCPlayer_0;
+
+    public static Player[] GetPlayers()
+    {
+      var players = GetPlayerManager()?.field_Private_List_1_Player_0;
+
+      if (players == null) {
+        return new Player[0];
+      } else {
+        lock (players) {
+          return players.ToArray();
+        }
+      }
+    }
+
+    public static GameObject GetAvatar(this Player player) => player.prop_VRCPlayer_0.prop_VRCAvatarManager_0.prop_GameObject_0;
+
+    public static bool IsLocal(this Player p) => p.name == GetLocalPlayer().name;
+  }
+}
